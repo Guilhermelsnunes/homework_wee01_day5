4ae8d0508bc3cc6f0308d7a18c1f80ef8ec0f932
Guilherme Nunes <guilhermelsnunes@gmail.com>
2021-11-12 15:45:03 +0000

    extra done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -51,3 +51,17 @@ def add_pet_to_customer(customer,new_pet):
     customer["pets"].append(new_pet)
 
 
+
+# -----optional----------
+
+
+
+def customer_can_afford_pet(customer,new_pet):
+    if customer["cash"] >= new_pet["price"]:
+        return True
+    else: return False
+
+
+# ----------
+
+
